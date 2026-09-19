2877d9322636bde1c2b45bc8f69b3b59fe92d1b4
omar-tawfik0 <omartawfik6633@gmail.com>
2026-09-19 07:16:11 +0300

    DONE

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 
 import { swaggerSpec } from "./config/swagger";
@@ -9,6 +10,13 @@ import authRoutes from "./routes/auth.routes";
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // ضيف هنا أي دومين هترفع الفرونت اند عليه بعدين
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -21,3 +29,4 @@ app.use(
 app.use("/api/auth", authRoutes);
 
 export default app;
+//install cors npm install --save-dev @types/cors
